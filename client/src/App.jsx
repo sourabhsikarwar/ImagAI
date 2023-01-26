f9e69fe8664f47de0a7f3dfdbf2abb5a936746b3
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2023-01-27 00:21:26 +0530

    Project Setup for Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl text-white bg-slate-800 p-4 text-center">
+      ImagAI - Your AI Powered Image generator and showcase platform
+    </div>
+  )
+}
+
+export default App
